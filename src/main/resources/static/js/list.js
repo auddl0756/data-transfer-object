e3f54e859634647b6fd5b7e6675de5a645fc1c87
auddl0756 <cjddl789123@naver.com>
2022-01-11 22:44:02 +0900

    fetch로 전달받은 post 데이터 렌더링

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -3,18 +3,30 @@ const get = 'get'
 
 init();
 
-function init(){
-    let data = requestAllPosts();
+async function init() {
+    let posts = await requestAllPosts();
+    console.log(posts);
+
+    let list1DOM = document.querySelector('.list1');
+
+
+    for (let post of posts) {
+        post.toString = function () {
+            return post.id + ' ' + post.title + ' ' + post.content;
+        }
+
+        list1DOM.innerHTML += '<li>' + post.toString() + '</li>';
+    }
 }
 
-function requestAllPosts(){
+function requestAllPosts() {
     return fetch(url, {
         method: get,
     })
         .then((response) => {
-            if(response.ok){
+            if (response.ok) {
                 return response.json();
-            }else{
+            } else {
                 return "something failed";
             }
         })
