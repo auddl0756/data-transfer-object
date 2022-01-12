d2fa67b4d08a920fb0661a72361329d442432979
auddl0756 <cjddl789123@naver.com>
2022-01-12 22:04:11 +0900

    request : post, body, string / controller : RequestBody String

---
+++ src/main/java/org/roon/dtos/web/api/ApiSampleController.java
@@ -0,0 +1,23 @@
+package org.roon.dtos.web.api;
+
+import org.roon.dtos.web.SampleController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/api/sample")
+@RestController
+public class ApiSampleController {
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
+    @PostMapping("/string")
+    public String sampleString(@RequestBody String str) {
+        logger.info(str);
+        return str;
+    }
+}
