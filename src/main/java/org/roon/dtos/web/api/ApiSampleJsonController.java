406cc2b9f0d8ce078b9af24e37f8534094068f01
auddl0756 <cjddl789123@naver.com>
2022-01-12 22:18:50 +0900

    request : post, body, int, content-type = json / controller : RequestBody int

---
+++ src/main/java/org/roon/dtos/web/api/ApiSampleJsonController.java
@@ -0,0 +1,21 @@
+package org.roon.dtos.web.api;
+
+import org.roon.dtos.web.SampleController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/sample/json")
+@RestController
+public class ApiSampleJsonController {
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
+    @PostMapping("/int")
+    public int sampleInt(@RequestBody int num) {
+        logger.info("param : " + num);
+        return num;
+    }
+}
